a0a91e6c7d1a1af3cf28b414658b6cf575996cf5
Igor Kropochev <xenos47@gmail.com>
2021-03-27 23:10:40 +0700

    test 1

--- testing-bash.sh
+++ testing-bash.sh
@@ -2,12 +2,40 @@
 
 var=11
 slots=3
+
+echo "var= ${var} slots= ${slots}"
+
 result=$((var / slots))
 k=$((var % slots ))
-for ((i=0; i<k; i++)); do
-  variableLengthArray[i]=$(( result + 1 ))
-done
-for ((i=k; i < slots; i++)); do
- variableLengthArray[i]=$result
-done
-echo "$result"
+
+echo "var/slots= ${result} remainder= ${k}"
+
+array=( one two three )
+
+printf "%s\n" "${array[@]}"
+
+
+#n=${#variableLengthArray[@]}
+#ratio=$(($var / $n))
+#rem=$(($var % $n))
+#for i in ${!variableLengthArray[@]} ; do
+#    variableLengthArray[$i]=$(( $ratio + ($i < $rem ? 1 : 0) ))
+#  
+#done
+#
+#printf "%s\n" "${variableLengthArray[@]}"
+
+#for ((i=0; i<k; i++)); do
+#
+#  variableLengthArray[i]=$(( result + 1 ))
+#  
+#  echo  "$variableLengthArray[i]"
+#
+#done
+#
+#for ((i=k; i < slots; i++)); do
+# variableLengthArray[i]=$result
+#done
+#
+#echo "$result"
+
